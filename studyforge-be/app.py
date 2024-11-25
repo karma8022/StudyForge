ca1f7264945cdf8e542c1708c459997360bf79e4
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2024-11-25 11:54:04 +0530

    Added Initial API Testing

--- studyforge-be/app.py
+++ studyforge-be/app.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Your Next.js frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 def read_root():
